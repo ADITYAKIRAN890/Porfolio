a2673b8388a2c3d434032fef227fc5b8176993ec
Aditya Kiran Shukla <adityashukla600700@gmail.com>
2026-05-08 11:46:25 +0530

    Hardcode reCAPTCHA Site Key to fix Vercel build missing env variable

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
+// The Site Key is safe to be public in the frontend code
+const RECAPTCHA_SITE_KEY = '6Lf0SN8sAAAAAHF5U4RqQC_fsabp0ZZZ60tg9RZa';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
       <App />
     </GoogleReCaptchaProvider>
   </StrictMode>,
